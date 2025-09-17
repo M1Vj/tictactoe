b79a803b896c7bc79e96224a7f223da7863c3254
Vj Mabansag <mabansagbj@gmail.com>
2025-09-17 12:30:47 +0800

    Add project file structure & simple page.tsx
    Also added simple Game component to text page.tsx

---
+++ app/tic-tac-toe/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './game/Game';
+
+
+export default function TicTacToe()
+{
+    return (
+        <div className="flex min-h-screen items-center justify-center">
+            <div className="rounded-2xl shadow p-6 border">
+                <Game />
+            </div>
+        </div>
+    )
+}
